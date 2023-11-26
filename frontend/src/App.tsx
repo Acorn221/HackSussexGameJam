c19ba5b3f96bd90a070d3d3bb4b9ac728f5ae791
James Arnott <jamesarnott2@gmail.com>
2023-11-26 11:53:21 +0000

    style: improved

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -48,7 +48,7 @@ const App = () => {
 
   return (
     <div className="flex justify-center align-middle h-screen">
-      <div className="bg-white text-black m-auto p-10 rounded-xl w-3/4 md:w-1/2 text-center flex flex-col">
+      <div className="bg-white text-black m-auto p-10 rounded-xl w-2/3 text-center flex flex-col bg-opacity-80 gap-2">
         <div className="underline text-5xl flex justify-center align-middle">
           <div className="flex m-auto">
             <img src={Logo} alt="logo" className="h-24 w-24" />
@@ -74,7 +74,6 @@ const App = () => {
         }
           </div>
         </div>
-        <Unity unityProvider={unityProvider} />
         <div className="flex-1 flex justify-center align-middle flex-col min-h-[30em]">
           {(!isLoading && !isAuthenticated) ? (
             <>
@@ -82,7 +81,7 @@ const App = () => {
               <LoginButton />
             </>
           ) : (
-            <iframe src="/game/index.html" title="game" className="flex-1 overflow-hidden" />
+            <iframe src="/game/index.html" title="game" className="flex-1 overflow-hidden" scrolling="no" />
           )}
         </div>
       </div>
