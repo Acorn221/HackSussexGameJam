ff6abdd98ae83b0f763a8cb1700f8e77670ed1ad
James Arnott <jamesarnott2@gmail.com>
2023-11-26 00:34:08 +0000

    style: improved

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './auth/LoginButton';
+import Logo from '../assets/chair v2_scaled_32x_pngcrushed.png';
 import '@/index.css';
 
 const App = () => {
@@ -8,33 +9,37 @@ const App = () => {
   return (
     <div className="flex justify-center align-middle h-screen">
       <div className="bg-white text-black m-auto p-10 rounded-xl w-3/4 md:w-1/2 text-center flex flex-col">
-        <div className="flex-auto flex align-middle">
-          <div className="flex-1" />
+        <div className="underline text-5xl flex justify-center align-middle">
+          <div className="flex m-auto">
+            <img src={Logo} alt="logo" className="h-24 w-24" />
+          </div>
+          <div className="flex-1 m-auto">
+            Rubiks Racer
+          </div>
           <div className="flex gap-4 justify-cemter align-middle">
             {
-          (!isLoading && !isAuthenticated) && (
-          <>
-            <p className="m-auto">Please login to continue</p>
-            <LoginButton />
-          </>
-          ) || user && isAuthenticated && (
+            user && isAuthenticated && (
             <>
-              <h2 className="m-auto">
-                Hi
-                {' '}
+              <h2 className="m-auto text-xl">
                 {user.name}
               </h2>
-              <img className="rounded-full h-12 w-12" src={user.picture} alt={user.name} />
+              <img className="rounded-full h-12 w-12 m-auto" src={user.picture} alt={user.name} />
             </>
-          )
+            )
         }
           </div>
         </div>
-        <div className="underline text-5xl">Retro Rubiks</div>
         <div className="flex-1 flex justify-center align-middle flex-col min-h-[30em]">
-          <div className="m-auto text-3xl">
-            INSERT GAME HERE
-          </div>
+          {(!isLoading && !isAuthenticated) ? (
+            <>
+              <p className="m-auto">Please login to continue</p>
+              <LoginButton />
+            </>
+          ) : (
+            <div className="m-auto text-3xl">
+              INSERT GAME HERE
+            </div>
+          )}
         </div>
       </div>
     </div>
--- frontend/src/auth/LoginButton.tsx
+++ frontend/src/auth/LoginButton.tsx
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button className="btn btn-primary" onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button className="btn bg-purple-700 text-white text-2xl hover:bg-purple-800 hover:scale-105" onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
